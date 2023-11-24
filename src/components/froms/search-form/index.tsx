ae529f4cf8f403ce41b5b5d2f3c0699079c4ecbc
Bohdan Hasyn <bodich163@gmail.com>
2023-11-24 21:05:57 +0200

    fix select styles

--- src/components/froms/search-form/index.tsx
+++ src/components/froms/search-form/index.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { Controller, useForm, useWatch } from "react-hook-form";
 import { z } from "zod";
-import { prioritySelectData, searchSchema, statusSelectData } from "./constants";
+import {
+    prioritySelectData,
+    searchSchema,
+    selectClassnamesConfig,
+    selectStylesConfig,
+    statusSelectData,
+} from "./constants";
 import { Input } from "@/components/input";
 import { useEffect } from "react";
 import { useDebounce } from "usehooks-ts";
-import Select, { ClassNamesConfig, StylesConfig } from "react-select";
-import { cn } from "@/utils/cn";
+import Select from "react-select";
 
 type SearchFormData = z.infer<typeof searchSchema>;
 
@@ -39,68 +44,6 @@ export const SearchForm = () => {
         }
         console.log(debouncedTitleValue, priorityValue, statusValue);
     }, [debouncedTitleValue, priorityValue, statusValue]);
-
-    const controlStyles = {
-        base: /*tw*/ " text-black border-2 outline-none border-friar-gray font-sans rounded-3xl bg-white hover:cursor-pointer h-[52px] w-32",
-        focus: /*tw*/ "border-friar-gray outline-none ",
-        nonFocus: /*tw*/ "border-gray-300 hover:border-friar-gray",
-    };
-    const placeholderStyles = /*tw*/ "pl-1 py-0.5 ";
-    const selectInputStyles = /*tw*/ "pl-1 py-0.5";
-    const valueContainerStyles = /*tw*/ "p-2";
-    const singleValueStyles = /*tw*/ "leading-7 text-center";
-    const indicatorsContainerStyles = /*tw*/ "p-1 ";
-    const indicatorSeparatorStyles = "none";
-    const dropdownIndicatorStyles = /*tw*/ "p-1 hover:bg-gray-100  rounded-2xl hover:text-black";
-    const menuStyles =
-        /*tw*/ "p-2 mt-2 border border-gray-200 bg-white rounded-3xl text-friar-gray animate-fadeIn ";
-    const groupHeadingStyles = /*tw*/ "ml-3 mt-2 mb-1  text-sm";
-    const optionStyles = {
-        base: /*tw*/ "hover:cursor-pointer px-3 py-2 rounded-3xl text-center",
-        focus: /*tw*/ "bg-gray-100 active:bg-gray-200",
-        selected:
-            /*tw*/ "text-black font-semibold after:content-['✔'] after:ml-2 after:text-green-500 ",
-    };
-    const noOptionsMessageStyles =
-        /*tw*/ " p-2 bg-gray-50 border border-dashed border-gray-200 rounded-sm";
-
-    const selectStylesConfig: StylesConfig<any, false, any> = {
-        input: (base) => ({
-            ...base,
-            "input:focus": {
-                boxShadow: "none",
-            },
-        }),
-        control: (base) => ({
-            ...base,
-        }),
-        dropdownIndicator: (base, state) => ({
-            ...base,
-            transition: "all .2s ease",
-            transform: state.selectProps.menuIsOpen ? "rotate(180deg)" : "none",
-        }),
-    };
-
-    const selectClassnamesConfig: ClassNamesConfig = {
-        control: ({ isFocused }) =>
-            cn(isFocused ? controlStyles.focus : controlStyles.nonFocus, controlStyles.base),
-        placeholder: () => placeholderStyles,
-        input: () => selectInputStyles,
-        valueContainer: () => valueContainerStyles,
-        singleValue: () => singleValueStyles,
-        indicatorsContainer: () => indicatorsContainerStyles,
-        indicatorSeparator: () => indicatorSeparatorStyles,
-        dropdownIndicator: () => dropdownIndicatorStyles,
-        menu: () => menuStyles,
-        groupHeading: () => groupHeadingStyles,
-        option: ({ isFocused, isSelected }) =>
-            cn(
-                isFocused && optionStyles.focus,
-                isSelected && optionStyles.selected,
-                optionStyles.base
-            ),
-        noOptionsMessage: () => noOptionsMessageStyles,
-    };
 
     return (
         <form onSubmit={onSubmit} className="flex items-center gap-5">
