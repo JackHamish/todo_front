a208d61f11b9ac17c28546e9ef7edbe461a04797
Bohdan Hasyn <bodich163@gmail.com>
2023-12-10 21:43:52 +0200

    test

--- src/domains/task/services.ts
+++ src/domains/task/services.ts
@@ -2,23 +2,18 @@ import { api } from "@/services/api";
 import { CreateTask, Task, TaskQueryParams, UpdateTask } from "./types";
 
 export async function getAllTasks(params: TaskQueryParams) {
-  return (await api.get<Task[]>("tasks", { params })).data;
+    return (await api.get<Task[]>("tasks", { params })).data;
 }
 
 export async function createTask(createTaskData: CreateTask) {
-  return (await api.post<Task>("tasks", createTaskData)).data;
+    return (await api.post<Task>("tasks", createTaskData)).data;
+    console.log("`");
 }
 
-export async function updateTask({
-  id,
-  data,
-}: {
-  id: string;
-  data: UpdateTask;
-}) {
-  return (await api.patch<Task>(`tasks/${id}`, data)).data;
+export async function updateTask({ id, data }: { id: string; data: UpdateTask }) {
+    return (await api.patch<Task>(`tasks/${id}`, data)).data;
 }
 
 export async function deleteTask(id: string) {
-  return (await api.delete<Task>(`tasks/${id}`)).data;
+    return (await api.delete<Task>(`tasks/${id}`)).data;
 }
